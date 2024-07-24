0ab182aafbc0ca2136dc00fb5d16781e5bf7130b
ahmedhgazy <ahmedmhgazy69@gmail.com>
2024-07-24 06:22:18 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather-forecasting';
+  usersList = ['Ahmed', 'Mohamed', 'Mahmoud', 'Hgazy'];
+  getListOfUsers() {
+    return this.usersList.slice();
+  }
+
+  ngOnInit(): void {
+    console.log('Hello ');
+  }
 }
