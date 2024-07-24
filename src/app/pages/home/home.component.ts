0ad17d92cde72c2354437dd3ae95410ab88dbcdd
ahmedhgazy <ahmedmhgazy69@gmail.com>
2024-07-24 12:57:47 +0300

    UI-final-version

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 import { WeatherDetailsComponent } from '../../components/weather-details/weather-details.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, SearchBarComponent, WeatherDetailsComponent],
+  imports: [
+    HeaderComponent,
+    SearchBarComponent,
+    WeatherDetailsComponent,
+    FooterComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Component,
+  HostListener,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.userScroll();
+    }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: any): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.userScroll();
+    }
+  }
+
+  userScroll(): void {
+    const navbar = document.querySelector('.navbar');
+
+    if (navbar) {
+      if (window.scrollY > 50) {
+        navbar.classList.add('border-bottom', 'navbar-sticky');
+      } else {
+        navbar.classList.remove(
+          'bg-primary',
+          'border-bottom',
+          'border-secondary',
+          'navbar-sticky'
+        );
+      }
+    }
+  }
 }
