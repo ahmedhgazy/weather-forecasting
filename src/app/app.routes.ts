72557ab30031393804672d3cd408580b69b3dbd7
ahmedhgazy <ahmedmhgazy69@gmail.com>
2024-07-24 14:58:10 +0300

    enhance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     component: HomeComponent,
   },
